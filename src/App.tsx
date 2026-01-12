a4c2156a4820e4cdd05ed203eadf93bc17b941e5
Plexi <stbnbasson@gmail.com>
2026-01-12 19:49:07 +0100

    cleanup: front cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-import {useState} from "react";
 import "./App.css";
-import {UsernameGate} from "./features/auth/UsernameGate";
-import {MainLayout} from "./layout/MainLayout";
-import {GamesList} from "./features/games/GamesList";
-import {AdminPage} from "./features/admin/AdminPage";
 
 function App() {
-    const isAdminRoute = typeof window !== "undefined" && window.location.pathname.startsWith("/admin");
-    const [username, setUsername] = useState<string | null>(null);
 
-    if (isAdminRoute) {
-        return <AdminPage/>;
-    }
-
-    if (!username) {
-        return <UsernameGate onUsernameSet={setUsername}/>;
-    }
-
-    return (
-        <div className="app-root">
-            <MainLayout username={username}>
-                <GamesList username={username}/>
-            </MainLayout>
-        </div>
-    );
+	return (
+		<div className="app-root">
+		</div>
+	);
 }
 
 export default App;
