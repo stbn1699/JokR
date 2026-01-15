3d79b1dd31da58102b1e8231005aef58603dcb9c
swan <stbnbasson@gmail.com>
2026-01-15 11:18:09 +0100

    front: creation of the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header/Header.tsx";
 
 function App() {
 
 	return (
 		<div className="app-root">
+            <Header></Header>
 		</div>
 	);
 }
