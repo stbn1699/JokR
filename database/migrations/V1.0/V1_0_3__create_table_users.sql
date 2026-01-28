d8728e63f1080be7660aef924a98352d03270765
swan <stbnbasson@gmail.com>
2026-01-28 15:39:09 +0100

    refacto > flyway : homogene

--- database/migrations/V1.0/V1_0_3__create_table_users.sql
+++ database/migrations/V1.0/V1_0_3__create_table_users.sql
@@ -1,8 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-CREATE TABLE IF NOT EXISTS users (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    username varchar(100) NOT NULL UNIQUE,
-    email varchar(255) NOT NULL UNIQUE,
-    password varchar(255) NOT NULL
+CREATE TABLE IF NOT EXISTS users
+(
+    id         uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    username   varchar(100) NOT NULL UNIQUE,
+    email      varchar(255) NOT NULL UNIQUE,
+    password   varchar(255) NOT NULL,
+    created_at TIMESTAMPTZ      DEFAULT now()
 );
